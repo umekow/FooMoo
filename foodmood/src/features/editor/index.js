0c82cfeb38f0d209547522a2b8997dc7f2a19d77
umekow <umekolashun@gmail.com>
2020-05-13 13:02:37 -0500

    added text area for header

--- foodmood/src/features/editor/index.js
+++ foodmood/src/features/editor/index.js
@@ -4,17 +4,27 @@ import 'react-quill/dist/quill.snow.css'
 
 
 const Editor = props => {
+    const [header, setHeader] = useState('Entry Title')
     const [text, setText] = useState('How are you feeling today?')
     return(
-        
-        <ReactQuill
-        style={{
-            width:'800px',
-            height: '400px', 
-            margin: '0 auto', 
-            
-        }}
-        theme='snow' value={text} onChange={setText}/>
+        <div className='editorContainer'>
+            {/* header for journal entry  */}
+            <div className='entryHeader'>
+                <input style={{width: '780px', height: '50px', margin:'0',  border: 'none'}} type='text' placeholder={header}/>
+            </div>
+            {/*text editor */}
+            <ReactQuill
+                style={{
+                    width:'800px',
+                    height: '400px', 
+                    margin: '0 auto', 
+                    
+                }}
+                theme='snow'
+                value={text}
+                onChange={setText}>
+            </ReactQuill>
+        </div>
         
     )
 }
