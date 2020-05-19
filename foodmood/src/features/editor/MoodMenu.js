10003e3e3be3e85efb345af884e83d2b4904b20e
umekow <umekolashun@gmail.com>
2020-05-18 20:47:03 -0500

    created a sub component for select menu

---
+++ foodmood/src/features/editor/MoodMenu.js
@@ -0,0 +1,21 @@
+import React from 'react'; 
+
+export default function MoodMenu(props){
+
+    return(
+        <div className="MoodMenu">
+
+        </div>
+    )
+}
+
+
+function MoodMenuItem(props){
+    return(
+        <div className='moodMenuItem' 
+            onClick={() => props.updateMood}>
+            <img className={moodImg} src={props.img} alt={props.mood}/>
+            <p className='moodLabel'>{props.mood}</p>
+        </div>
+    )
+}
