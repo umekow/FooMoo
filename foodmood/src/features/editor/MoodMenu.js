db735e5e9e3fa79e895d4e92f9b8d579e7c94b6c
Restyled.io <commits@restyled.io>
2020-05-19 01:52:30 +0000

    Restyled by prettier

--- foodmood/src/features/editor/MoodMenu.js
+++ foodmood/src/features/editor/MoodMenu.js
@@ -1,20 +1,14 @@
-import React from 'react';
+import React from "react";
 
 export default function MoodMenu(props) {
-
-  return (
-      <div className = "MoodMenu">
-
-      </div>
-    )
+  return <div className="MoodMenu"></div>;
 }
 
-
-function MoodMenuItem(props){
-    return(
-        <div className='moodMenuItem' 
-            onClick={() => props.updateMood}>
-            <img className={moodImg} src={props.img} alt={props.mood}/>
-      <p className = 'moodLabel'>{props.mood}</p>
-        </div>)
+function MoodMenuItem(props) {
+  return (
+    <div className="moodMenuItem" onClick={() => props.updateMood}>
+      <img className={moodImg} src={props.img} alt={props.mood} />
+      <p className="moodLabel">{props.mood}</p>
+    </div>
+  );
 }
